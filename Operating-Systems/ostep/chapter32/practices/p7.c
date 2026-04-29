4d5d188e77ddedaa1623ef7184582154c11a4d50
Yush1nk1m <kys010306@sogang.ac.kr>
2026-04-29 10:15:02 +0900

    20260429: OSTEP chapter 32 practices 정리
    - Locked Data Structures 연습 코드 작성

---
+++ Operating-Systems/ostep/chapter32/practices/p7.c
@@ -0,0 +1,24 @@
+#include "common_threads.h"
+#include "p4.c"
+
+#define BUCKETS 16
+
+typedef struct __hash_t {
+    list_t lists[BUCKETS];
+} hash_t;
+
+void Hash_Init(hash_t* H) {
+    for (int i = 0; i < BUCKETS; ++i) {
+        List_Init(&H->lists[i]);
+    }
+}
+
+int Hash_Insert(hash_t* H, int key) {
+    int bucket = key % BUCKETS;
+    return List_Insert(&H->lists[bucket], key);
+}
+
+int Hash_Lookup(hash_t* H, int key) {
+    int bucket = key % BUCKETS;
+    return List_Lookup(&H->lists[bucket], key);
+}
